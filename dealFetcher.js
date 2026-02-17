cf77bb96ada4fe9f468f6e6bb5d93b14eec3149e
markrkoeppen-netizen <mark.r.koeppen@gmail.com>
2026-02-17 14:00:01 -0600

    Update dealFetcher.js

--- dealFetcher.js
+++ dealFetcher.js
@@ -2,17 +2,17 @@ const axios = require('axios');
 const { getFirestore } = require('./firebase');
 
 const PRIORITY_BRANDS = [
-  'Abercrombie & Fitch', 'Adidas', 'Allbirds', 'Alo', 'Athleta', 'BIRKENSTOCK',
-  'Bombas', 'Brooks Brothers', 'Burberry', 'Burlebo', 'Chloé', 'Christian Louboutin',
-  'Chubbies', 'Coach', 'Cole Haan', 'Columbia', 'Costa', 'Crocs', 'Cult Gaia',
+  'Abercrombie & Fitch', 'Adidas', 'Allbirds', 'Alo', 'Ariat', 'Athleta', 'BIRKENSTOCK',
+  'Bombas', 'Brooks Brothers', 'Burberry', 'Burlebo', 'Carhartt', 'Chloé', 'Christian Louboutin',
+  'Chubbies', 'Cinch', 'Coach', 'Cole Haan', 'Columbia', 'Costa', 'Crocs', 'Cruel Girl', 'Cult Gaia',
   'Dacor', 'Dolce & Gabbana', 'Donna Karan', 'Estée Lauder', 'Fendi', 'Feragamo',
-  'Gucci', 'Havaianas', 'Hermès', 'Jimmy Choo', 'Kate Spade', 'Kendra Scott', 'Kith', 'Lacoste',
-  'Louis Vuitton', 'Lucchese', 'Lululemon', 'Lush', 'Madewell', 'Marc Jacobs',
-  'Michael Kors', 'Nike', 'Oakley', 'OluKai', 'On Running', 'OOFOS', 'Oscar de la Renta', 'Polo Ralph Lauren',
-  'Poncho Outdoors', 'Prada', 'Ray-Ban', 'Reef', 'Rhone', 'Saint Laurent', 'Sanuk', 'Stuart Weitzman',
-  'Teva', 'The North Face', 'The Row', 'Thom Browne', 'Tom Ford', 'Tommy Bahama',
+  'Gucci', 'Havaianas', 'Hermès', 'Jimmy Choo', 'Justin Boots', 'Kate Spade', 'Kendra Scott', 'Kith', 'Lacoste',
+  'Levi Strauss', 'Louis Vuitton', 'Lucchese', 'Lululemon', 'Lush', 'Madewell', 'Marc Jacobs',
+  'Michael Kors', 'Nike', 'Oakley', 'OluKai', 'On Running', 'OOFOS', 'Oscar de la Renta', 'Panhandle Slim', 'Polo Ralph Lauren',
+  'Poncho Outdoors', 'Prada', 'Ray-Ban', 'Reef', 'Rhone', 'Saint Laurent', 'Sanuk', 'Stetson', 'Stuart Weitzman',
+  'Teva', 'The North Face', 'The Row', 'Thom Browne', 'Tom Ford', 'Tommy Bahama', 'Tony Lama',
   'Tory Burch', 'TravisMatthew', 'Tumi', 'UGG', 'Under Armour', 'Vera Wang',
-  'Vineyard Vines', 'Vuori', 'Yeti', 'YoungLA'
+  'Vineyard Vines', 'Vuori', 'Wrangler', 'Yeti', 'YoungLA'
 ];
 
 async function searchDealsForBrand(brandName) {
